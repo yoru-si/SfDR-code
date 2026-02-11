211cc203ee7a02e28861a9140fc8161e3b92cf45
Raghav <raghavsri04@gmail.com>
2026-02-12 00:26:45 +0100

    Assignment1.2

---
+++ assignment1/assignment1.2/include/BMI.h
@@ -0,0 +1,23 @@
+/*
+file: BMI.h
+Author: Raghavendran Srinivasan(s3656721), Srinkrishnan Srinivasan(s3603288SS)
+Group number: SDfR-PPD 16
+
+Description: This is the header file for the BMI calculator. It contains the function prototype for evaluating
+*/
+
+#ifndef divide_h
+#define divide_h
+
+
+
+/* Calculates the Body Mass Index (BMI), returns a decimal value */
+double calculateBMI(double weight, double height);
+
+/*Determines BMI category text, prints text of the datatype string*/
+void evaluateAndPrintBMI(double bmi);
+
+/*Prints the information chart, no input arguments, prints the information chart*/
+void printInfo();
+
+#endif
