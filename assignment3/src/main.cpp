425c46b8c8fced8477e13c380c1b4dd9b383280f
Srikrishnan Srinivasan <93307384+yoru-si@users.noreply.github.com>
2026-03-02 21:51:35 +0100

    assignment 3

---
+++ assignment3/src/main.cpp
@@ -0,0 +1,38 @@
+//==============================================================
+// Filename    : main.cpp    
+// Authors     : Srikrishnan Srinivasan (s3603288), Raghavendran Srinivasan (s)
+// Group       : Group 16
+// License     : N.A. or opensource license like LGPL
+// Description : Main File
+//==============================================================
+#include <iostream>
+#include "ListNode.h"
+#include "List.h"
+#include <string>
+
+void fillList(List<char>& myList){
+    std::string userInput;
+    std::cout << "Enter input string: \n";
+    std::getline(std::cin, userInput);
+    for (char letter : userInput){
+        myList.insertAtBack(letter);
+    }
+}
+
+
+
+int main() {
+    List<char> newlist1;
+    List<char> newlist2;
+    fillList(newlist1);
+    fillList(newlist2);
+    newlist1.print();
+    newlist2.print();
+    List<char> conlist1;
+    List<char> conlist2;
+    fillList(conlist1);
+    fillList(conlist2);
+    conlist1.concatenate(conlist2);
+    conlist1.print();
+    return 0;
+}
