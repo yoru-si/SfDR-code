bc0b223b432e269d9de1f7cbef2fa95dbc1a631e
Raghav <raghavsri04@gmail.com>
2026-03-12 03:47:05 +0100

    Readme file and Edge Cases

---
+++ assignment4/Readme.txt
@@ -0,0 +1,20 @@
+Assignment 4 - Inheritance
+________________________________
+
+1. Build Command:
+g++ src/*.cpp -iquote include -o o.exe
+
+2. Run Command
+Windows: o.exe
+
+3. What it does:
+- Contains the package information hard-coded.
+- If any vital information is found Invalud, asks for re-entering the details as user input.
+- Calculates the cost depending on type of package.
+- Prints the Cost with Sender and Reciever name.
+
+
+Locate files:
+- Structure of Classes and Function Prototypes include/Package.h
+- Function Declarations include/Package.cpp
+- Main file  src/main.cpp
--- assignment4/src/main.cpp
+++ assignment4/src/main.cpp
@@ -25,16 +25,24 @@ int main() {
     packages.push_back(new TwoDayPackage("Alice", "123 Main St", "Bob", "456 Elm St", 10));
     packages.push_back(new TwoDayPackage("Charlie", "789 Oak St", "Dave", "321 Pine St", 5));
     packages.push_back(new OvernightPackage("Eve", "654 Maple St", "Frank", "987 Cedar St", 3));
+    printCosts(packages);
 
-    cout << "\n test case\n";
+    cout << "\nEdge test cases\n";
+    packages.push_back(new TwoDayPackage("SmallSender", "1 Start St", "SmallReceiver", "1 End St", 0.1));
+    packages.push_back(new OvernightPackage("SmallSender", "1 Start St", "SmallReceiver", "1 End St", 0.1));
+    packages.push_back(new TwoDayPackage("LargeSender", "100 Start St", "LargeReceiver", "100 End St", 100));
+    packages.push_back(new OvernightPackage("LargeSender", "100 Start St", "LargeReceiver", "100 End St", 100));
+    printCosts(packages);
+
+    cout << "\n Invalid cases\n";
     // negative weight test case
     packages.push_back(new TwoDayPackage("Charlie", "789 Oak St", "Dave", "321 Pine St", -5));
 
-    cout << "\n test case\n";
+    cout << "\n Invalid case\n";
     // empty string test case
     packages.push_back(new OvernightPackage("", "111 Blank Ave", "", "222 Nowhere Rd", 2));
 
-    cout << "\n test case\n";
+    cout << "\n Invalid case\n";
     // identical addresses test case
     packages.push_back(new TwoDayPackage("Gary", "Same House St", "Gina", "Same House St", 4));
 
